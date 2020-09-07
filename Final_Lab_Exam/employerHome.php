6dc9c5ed2b4218d52e14ca719126fd34b032a46a
RaditTahmid <17-34536-2@student.aiub.edu>
2020-09-07 13:34:13 +0600

    Employee Login Bug Fixed and Logout added

--- Final_Lab_Exam/employerHome.php
+++ Final_Lab_Exam/employerHome.php
@@ -11,7 +11,8 @@ session_start();
     <center>
     <?php
         echo "<h2> Welcome ".$_SESSION['userName']."!</h2>";
-    ?>
+    ?> 
+    <a href="logout.php">Logout</a>
     </center>
     
 </body>
